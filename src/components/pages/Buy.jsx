42eff5a908eadcb98692317d8f54c892a9b5e1d9
ranelya <149768902+ranelya@users.noreply.github.com>
2024-05-17 15:56:10 +0600

    fix

--- src/components/pages/Buy.jsx
+++ src/components/pages/Buy.jsx
@@ -14,6 +14,7 @@ import {
   useDisclosure,
   useToast,
 } from "@chakra-ui/react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 const Buy = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
